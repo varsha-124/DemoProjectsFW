bc0c7eb96d225df56a6679829a62a49cf0ae30d2
mendon-varsha <mendonvarsha11@gmail.com>
2025-01-03 14:48:22 +0530

    updated

--- DWSProjectFrameWork/src/test/java/contact/CreateContact.java
+++ DWSProjectFrameWork/src/test/java/contact/CreateContact.java
@@ -8,6 +8,7 @@ public static void main(String[] args) {
 	System.out.println("ashika added new contact");
 
 	System.out.println("New contact created");
+	System.out.println("Contact is updated");
 
 }
 }
