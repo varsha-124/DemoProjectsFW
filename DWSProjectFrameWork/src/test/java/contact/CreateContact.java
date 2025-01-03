7ad600eb8579c7d749c72a1ab350c2067f44faa9
mendon-varsha <mendonvarsha11@gmail.com>
2025-01-03 15:20:05 +0530

    New method added

--- DWSProjectFrameWork/src/test/java/contact/CreateContact.java
+++ DWSProjectFrameWork/src/test/java/contact/CreateContact.java
@@ -9,6 +9,8 @@ public static void main(String[] args) {
 
 	System.out.println("New contact created");
 	System.out.println("Contact is updated");
+	
+	System.out.println("New Method Added");
 
 }
 }
