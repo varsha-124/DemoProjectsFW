62164218c5968a1d2f5d886fb4584d169b22223e
mendon-varsha <mendonvarsha11@gmail.com>
2025-01-03 11:04:16 +0530

    new contact created

--- DWSProjectFrameWork/src/test/java/contact/CreateContact.java
+++ DWSProjectFrameWork/src/test/java/contact/CreateContact.java
@@ -4,5 +4,6 @@ public class CreateContact {
 public static void main(String[] args) {
 	System.out.println("Contact created");
 	//AE modified the script
+	System.out.println("New contact created");
 }
 }
