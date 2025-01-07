dcf07f8358ce1d1261271d2821bf2964eef357ab
rashminavale1606 <rashminavale1606@gmail.com>
2025-01-07 17:16:56 +0530

    committed name and profession

--- DWSProjectFrameWork/src/test/java/org/CreateOrg.java
+++ DWSProjectFrameWork/src/test/java/org/CreateOrg.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("Org Created");
 	System.out.println("org details added");
 	System.out.println("TekPyramid is leading org");
+	System.out.println("Name: Harish Proffession: HR");
 }
 }
